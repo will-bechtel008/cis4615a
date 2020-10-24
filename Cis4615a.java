ec7642e580e877de4b80a6526e63482dcfc26abd
will-bechtel008 <54552139+will-bechtel008@users.noreply.github.com>
2020-10-24 14:15:51 -0400

    Added Hello, World function to edits

---
+++ Cis4615a.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ * - Added " - from the class of CIS4615 - Fall 2019 - UCF"
+ *  % java Cis4615a
+ *  Hello, World - from the class of CIS4615 - Fall 2019 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4615a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Willam Bechtel");
+    }
+
+}
